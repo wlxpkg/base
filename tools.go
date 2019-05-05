575cbbfca0f7e44ace379bbe40f4fbee19030632
Yabo <386139859@qq.com>
2019-05-05 17:21:12 +0800

    :art: 添加自定义时间格式

--- tools.go
+++ tools.go
@@ -7,8 +7,10 @@
 package pkg
 
 import (
+	"database/sql/driver"
 	"fmt"
 	"runtime/debug"
+	"time"
 )
 
 func R(data interface{}, name string) {
@@ -19,4 +21,34 @@ func R(data interface{}, name string) {
 func D(data interface{}) {
 	fmt.Printf("%s\n", debug.Stack())
 	fmt.Printf("%+v\n", data)
+}
+
+// JSONTime format json time field by myself
+type JSONTime struct {
+	time.Time
+}
+
+// MarshalJSON on JSONTime format Time field with Y-m-d H:i:s
+func (t JSONTime) MarshalJSON() ([]byte, error) {
+	formatted := fmt.Sprintf("\"%s\"", t.Format("2006-01-02 15:04:05"))
+	return []byte(formatted), nil
+}
+
+// Value insert timestamp into mysql need this function.
+func (t JSONTime) Value() (driver.Value, error) {
+	var zeroTime time.Time
+	if t.Time.UnixNano() == zeroTime.UnixNano() {
+		return nil, nil
+	}
+	return t.Time, nil
+}
+
+// Scan value of time.Time
+func (t *JSONTime) Scan(v interface{}) error {
+	value, ok := v.(time.Time)
+	if ok {
+		*t = JSONTime{Time: value}
+		return nil
+	}
+	return fmt.Errorf("can not convert %v to timestamp", v)
 }
