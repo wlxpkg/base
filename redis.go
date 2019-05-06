94ba63d136a4a24514e0878002aa5c35ed1c28b7
qiuling <mynameisql@gmail.com>
2019-05-06 10:53:17 +0800

    redis

--- redis.go
+++ redis.go
@@ -1,7 +1,9 @@
 package pkg
 
 import (
+	"encoding/json"
 	"strconv"
+	"time"
 
 	"github.com/go-redis/redis"
 	"github.com/gookit/config"
@@ -13,11 +15,33 @@ type Redis struct {
 var Client *redis.Client
 
 func init() {
-	db, _ := strconv.Atoi(config.GetEnv("REDIS_SELECT"))
+	db, _ := strconv.Atoi(config.GetEnv("REDIS_SELECT", "1"))
 	Client = redis.NewClient(&redis.Options{
-		Addr:     config.GetEnv("REDIS_HOST") + ":" + config.GetEnv("REDIS_PORT"),
-		Password: config.GetEnv("REDIS_PASSWORD"),
+		Addr:     config.GetEnv("REDIS_HOST", "127.0.0.1") + ":" + config.GetEnv("REDIS_PORT", "3306"),
+		Password: config.GetEnv("REDIS_PASSWORD", ""),
 		DB:       db,
 		PoolSize: 100,
 	})
+}
+
+func Set(key string, value interface{}, ttl int) {
+	switch value.(type) {
+	case string:
+		value = value
+	case array:
+		value = json.Marshal(value)
+	}
+	err := Client.Set(key, value, time.Duration(ttl)*time.Second).Err()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Get(key string) string {
+	val, err := Client.Get(key).Result()
+	if err != nil {
+		panic(err)
+	}
+
+	return val
 }
