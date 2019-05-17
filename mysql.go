9270267b49a95616d3fba9b655d3d70b563738ef
qiuling <mynameisql@gmail.com>
2019-05-17 18:46:29 +0800

    尝试 orm 重连

--- mysql.go
+++ mysql.go
@@ -2,42 +2,44 @@ package pkg
 
 import (
 	. "artifact/pkg/config"
+	"artifact/pkg/log"
 	"bytes"
-	"fmt"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
 
-var DB *gorm.DB
+var mysqlLink = bytes.NewBufferString("")
 
 func init() {
-	var err error
-	mysqlLink := bytes.NewBufferString("")
-
 	mysqlLink.WriteString(Config.Mysql.Username)
 	mysqlLink.WriteString(":" + Config.Mysql.Password + "@tcp")
 	mysqlLink.WriteString("(" + Config.Mysql.Host)
 	mysqlLink.WriteString(":" + Config.Mysql.Port + ")")
 	mysqlLink.WriteString("/" + Config.Mysql.Database)
 	mysqlLink.WriteString("?charset=utf8&parseTime=True&loc=Local&timeout=100ms")
-
-	DB, err = gorm.Open("mysql", mysqlLink.String())
-	if err != nil {
-		fmt.Printf("\nmysql connect err %v\n", err)
-	}
-
-	if DB.Error != nil {
-		fmt.Printf("\ndatabase err %v\n", DB.Error)
-	}
-	// set table prefix
-	// gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
-	// 	return "course_" + defaultTableName
-	// }
-
-	// 全局禁用表名复数 TableName不受影响
-	DB.SingularTable(true)
-
-	DB.DB().SetMaxIdleConns(100)
-	//DB.DB().SetMaxOpenConns(1000)
 }
+
+func newDB() (orm *gorm.DB) {
+	// var orm *gorm.DB
+	var err error
+
+	for orm, err = gorm.Open("mysql", mysqlLink.String()); err != nil; {
+		log.Err("mysql connect err: " + err.Error())
+		time.Sleep(5 * time.Second)
+		orm, err = gorm.Open("mysql", mysqlLink.String())
+	}
+
+	if orm.Error != nil {
+		log.Err("database err: " + orm.Error.Error())
+	}
+	// 全局禁用表名复数 TableName不受影响
+	orm.SingularTable(true)
+	orm.DB().SetMaxIdleConns(100)
+	//orm.DB().SetMaxOpenConns(1000)
+
+	return
+}
+
+var DB = newDB()
