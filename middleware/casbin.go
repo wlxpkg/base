db6bbb73520fdd8d6444c0a1bcd25c84b7e17953
qiuling <mynameisql@gmail.com>
2019-06-17 19:11:43 +0800

    :construction: Casbin middleware

--- middleware/casbin.go
+++ middleware/casbin.go
@@ -2,7 +2,43 @@
  * @Author: qiuling
  * @Date: 2019-06-17 15:33:04
  * @Last Modified by: qiuling
- * @Last Modified time: 2019-06-17 16:23:08
+ * @Last Modified time: 2019-06-17 19:10:16
  */
 
 package middleware
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Casbin() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		uid := getUser(c)
+
+		fmt.Printf("%+v\n", uid)
+
+		// 设置 example 变量
+		c.Set("example", uid)
+
+		c.Next()
+	}
+}
+
+func getUser(c *gin.Context) (err error) {
+	authorization := c.GetHeader("authorization")
+	jwt := strings.TrimPrefix(authorization, "Bearer ")
+
+	fmt.Printf("%+v\n", jwt)
+
+	if jwt == "" {
+		err = errors.New("ERR_INVALID_TOKEN")
+	}
+	return
+	// method := r.Method
+	// path := r.URL.Path
+}
