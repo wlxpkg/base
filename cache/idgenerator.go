d4b0a53ff972c895f1edf0af26eb46e8533fb941
qiuling <mynameisql@gmail.com>
2019-06-17 17:30:47 +0800

    修改 id生成器, 用redis 客户端到新的id生成器取id

---
+++ cache/idgenerator.go
@@ -0,0 +1,72 @@
+/*
+ * @Author: qiuling
+ * @Date: 2019-06-17 15:57:14
+ * @Last Modified by: qiuling
+ * @Last Modified time: 2019-06-17 16:57:07
+ */
+package cache
+
+import (
+	. "artifact/pkg/config"
+	"artifact/pkg/log"
+	"errors"
+	"strings"
+
+	"github.com/go-redis/redis"
+)
+
+// type Idgenerator struct {
+// }
+
+var idgeneratorClient *redis.Client
+
+func init() {
+	idgeneratorClient = redis.NewClient(&redis.Options{
+		Addr:     Config.Idgenerator.Host + ":" + Config.Idgenerator.Port,
+		Password: "",
+		DB:       0,
+		PoolSize: 100,
+	})
+}
+
+/* func NewCache() (cache *Cache) {
+	cache = &Cache{
+		autoPrefix: true,
+		prefix:     Config.Redis.Prefix,
+	}
+	return
+} */
+
+func Getid() (value string, err error) {
+	value, err = idgeneratorClient.Do("getid").String()
+
+	if err != nil {
+		log.Warn(err)
+		return
+	}
+
+	if strings.Contains(value, "ERROR") {
+		log.Warn(err)
+		err = errors.New("ERR_IDGEN_FAIL")
+		return
+	}
+
+	return
+}
+
+func Batchid(num int) (value string, err error) {
+	value, err = idgeneratorClient.Do("batchid", num).String()
+
+	if err != nil {
+		log.Warn(err)
+		return
+	}
+
+	if strings.Contains(value, "ERROR") {
+		log.Warn(err)
+		err = errors.New("ERR_IDGEN_FAIL")
+		return
+	}
+
+	return
+}
---
+++ middleware/casbin.go
@@ -0,0 +1,8 @@
+/*
+ * @Author: qiuling
+ * @Date: 2019-06-17 15:33:04
+ * @Last Modified by: qiuling
+ * @Last Modified time: 2019-06-17 16:23:08
+ */
+
+package middleware
