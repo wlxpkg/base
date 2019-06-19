69d110b01a59ee7491de55c0fcf47139d87f13d8
xss <person@xhsui.com>
2019-06-19 10:34:47 +0800

    fix 随机字符串

--- tools.go
+++ tools.go
@@ -84,13 +84,13 @@ func Unix2Time(stamp int64) time.Time {
 }
 
 // 生成随机字符串
-func RandStr(len int64) string {
-   str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-   bytes := []byte(str)
-   result := []byte{}
-   r := rand.New(rand.NewSource(time.Now().UnixNano()))
-   for i := 0; i < len; i++ {
-      result = append(result, bytes[r.Intn(len(bytes))])
-   }
-   return string(result)
+func RandStr(l int) string {
+	str := "0123456789abcdefghijklmnopqrstuvwxyz"
+	bytes := []byte(str)
+	result := []byte{}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < l; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
 }
