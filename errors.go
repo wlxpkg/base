58dc134239835ccd976ad267ffacd79098d21e27
xss <person@xhsui.com>
2019-07-31 17:07:06 +0800

    fix

--- errors.go
+++ errors.go
@@ -36,15 +36,20 @@ var Errs = map[string]Errors{
 	"ERR_GET_FAIL":               Errors{25002, "获取视频异常"},
 
 	"ERR_ORDER_NOT_EXIST":            Errors{21000, "订单不存在"},
-	"ERR_ORDER_ALREADY_PAY":          {21001, "订单已经支付"},
-	"ERR_ORDER_TIME_OUT":             {21002, "订单已经超时，请重新下单"},
-	"ERR_ORDER_RESET_TYPE_NOT_EXIST": {21003, "重置订单的支付类型不存在"},
+	"ERR_ORDER_ALREADY_PAY":          Errors{21001, "订单已经支付"},
+	"ERR_ORDER_TIME_OUT":             Errors{21002, "订单已经超时，请重新下单"},
+	"ERR_ORDER_RESET_TYPE_NOT_EXIST": Errors{21003, "重置订单的支付类型不存在"},
 
-	"ERR_LINK_APPLY_AGENT_INVALD":        {21101, "申请代理的链接失败"},
-	"ERR_LINK_APPLY_AGENT_ALREADY_AGENT": {21102, "申请代理的用户已经是代理用户"},
+	"ERR_LINK_APPLY_AGENT_INVALD":        Errors{21101, "申请代理的链接失败"},
+	"ERR_LINK_APPLY_AGENT_ALREADY_AGENT": Errors{21102, "申请代理的用户已经是代理用户"},
 
-	"ERR_WALLET_PAY_FAIL": {22000, "钱包支付失败"},
-	"ERR_WALLET_PAY_EXP":  {22001, "钱包数额不正确"},
+	"ERR_WALLET_PAY_FAIL": Errors{22000, "钱包支付失败"},
+	"ERR_WALLET_PAY_EXP":  Errors{22001, "钱包数额不正确"},
+
+	"ERR_GOODS_EXIST":       Errors{23000, "商品已存在"},
+	"ERR_GOODS_NOT_EXIST":   Errors{23001, "商品不存在"},
+	"ERR_GOODS_OFF_SHELVES": Errors{23002, "商品已下架"},
+	"ERR_TIME_EXPIRE":       Errors{23004, "时间设置不正确"},
 }
 
 func Excp(errString string) error {
