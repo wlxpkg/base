fe081ba67e007bcbeac00df533ac66f3d25607a9
Yabo <386139859@qq.com>
2019-12-05 14:28:25 +0800

    :art: 修改结构体字段属性

--- req/http.go
+++ req/http.go
@@ -34,20 +34,20 @@ const (
 )
 
 type HttpClient struct {
-	baseUrl string
-	header  map[string]string
-	data    map[string]string
-	client  *http.Client
+	BaseURL string
+	Header  map[string]string
+	Data    map[string]string
+	Client  *http.Client
 }
 
 // init HTTPClient
 func NewClient(baseUrl string) *HttpClient {
 	c := new(HttpClient)
 
-	c.baseUrl = baseUrl
-	c.header = make(map[string]string)
-	c.data = make(map[string]string)
-	c.client = createHTTPClient()
+	c.BaseURL = baseUrl
+	c.Header = make(map[string]string)
+	c.Data = make(map[string]string)
+	c.Client = createHTTPClient()
 	return c
 }
 
@@ -68,42 +68,42 @@ func createHTTPClient() *http.Client {
 	return client
 }
 
-// SetBaseUrl 设置基础地址
-func (c *HttpClient) SetBaseUrl(baseUrl string) *HttpClient {
-	c.baseUrl = baseUrl
+// SetBaseURL 设置基础地址
+func (c *HttpClient) SetBaseURL(baseURL string) *HttpClient {
+	c.BaseURL = baseURL
 	return c
 }
 
 // AddHeader 添加一个 header
 func (c *HttpClient) AddHeader(name string, value string) *HttpClient {
-	c.header[name] = value
+	c.Header[name] = value
 	return c
 }
 
 // AddHeaders 添加一组 header 数据
 func (c *HttpClient) AddHeaders(headers map[string]string) *HttpClient {
 	for key, header := range headers {
-		c.header[key] = header
+		c.Header[key] = header
 	}
 	return c
 }
 
 // SetData 设置查询数据
 func (c *HttpClient) SetData(data map[string]string) *HttpClient {
-	c.data = data
+	c.Data = data
 	return c
 }
 
 // reset 重置 header 和 data
 func (c *HttpClient) reset() {
-	c.header = make(map[string]string)
-	c.data = make(map[string]string)
+	c.Header = make(map[string]string)
+	c.Data = make(map[string]string)
 }
 
 // Request 发送 http 请求
 func (c *HttpClient) Request(method string, route string) (resp string, err error) {
 
-	if c.baseUrl == "" {
+	if c.BaseURL == "" {
 		err = errors.New("ERR_HTTP_BASEURL")
 		return
 	}
@@ -122,7 +122,7 @@ func (c *HttpClient) Request(method string, route string) (resp string, err erro
 	}
 	reqest = c.buildHeader(reqest, method)
 
-	response, err := c.client.Do(reqest) //发送请求
+	response, err := c.Client.Do(reqest) //发送请求
 	c.reset()
 	if err != nil {
 		log.Err(err)
@@ -148,23 +148,23 @@ func (c *HttpClient) Request(method string, route string) (resp string, err erro
 func (c *HttpClient) buildData(method string, route string) (requrl string, body *strings.Reader, err error) {
 	v := url.Values{}
 	body = strings.NewReader("")
-	requrl = c.baseUrl + route
-	if len(c.data) != 0 {
+	requrl = c.BaseURL + route
+	if len(c.Data) != 0 {
 		switch method {
 		case "GET":
 			query := url.Values{}
-			for k, v := range c.data {
+			for k, v := range c.Data {
 				query.Add(k, v)
 			}
 			requrl += "?" + query.Encode()
 		case "FORM":
-			for key, data := range c.data {
+			for key, data := range c.Data {
 				v.Set(key, data)
 			}
 
 			body = strings.NewReader(v.Encode())
 		default:
-			jsonBype, jsonErr := json.Marshal(c.data)
+			jsonBype, jsonErr := json.Marshal(c.Data)
 			if jsonErr != nil {
 				err = jsonErr
 				return
@@ -178,8 +178,8 @@ func (c *HttpClient) buildData(method string, route string) (requrl string, body
 }
 
 func (c *HttpClient) buildHeader(reqest *http.Request, method string) *http.Request {
-	if len(c.header) != 0 {
-		for k, h := range c.header {
+	if len(c.Header) != 0 {
+		for k, h := range c.Header {
 			reqest.Header.Set(k, h)
 		}
 	}
